b2804177ddb37f58547232db79e86f6f2eacd0fb
VynoDePal <odjokevyn@gmail.com>
2024-06-01 17:38:55 +0100

    feat: Error management and unit testing of user management

--- app/src/Controller/Authentification/AuthController.php
+++ app/src/Controller/Authentification/AuthController.php
@@ -16,6 +16,7 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Nelmio\ApiDocBundle\Annotation\Model;
 use OpenApi\Attributes as OA;
 use Symfony\Component\Serializer\SerializerInterface;
+use Psr\Log\LoggerInterface;
 
 #[Route('/api')]
 class AuthController extends AbstractController
@@ -23,12 +24,14 @@ class AuthController extends AbstractController
     private TokenService $tokenService;
     private UserService $userService;
     private UserPasswordHasherInterface $passwordHasher;
+    private LoggerInterface $logger;
 
-    public function __construct(UserPasswordHasherInterface $passwordHasher, TokenService $tokenService, UserService $userService)
+    public function __construct(UserPasswordHasherInterface $passwordHasher, TokenService $tokenService, UserService $userService, LoggerInterface $logger)
     {
         $this->tokenService = $tokenService;
         $this->userService = $userService;
         $this->passwordHasher = $passwordHasher;
+        $this->logger = $logger;
     }
     /**
      * Enregistre un nouvel utilisateur.
@@ -41,33 +44,42 @@ class AuthController extends AbstractController
     #[OA\Response(response: 400, description: 'Invalid data')]
     public function register(Request $request, EntityManagerInterface $entityManager): JsonResponse
     {
-        $data = json_decode($request->getContent(), true);
-        $user = new User();
-        
-        // Vérifier que tous les champs nécessaires sont présents
-        if (!isset($data['login'], $data['password'], $data['email'], $data['firstname'], $data['lastname'])) {
-            return new JsonResponse(['error' => 'Missing required fields'], Response::HTTP_BAD_REQUEST);
+        $this->logger->info('Inscription de l\'utilisateur');
+        try {
+            $data = json_decode($request->getContent(), true);
+            $this->logger->debug('Request data', ['data' => $data]);
+            $user = new User();
+            
+            // Vérifier que tous les champs nécessaires sont présents
+            if (!isset($data['login'], $data['password'], $data['email'], $data['firstname'], $data['lastname'])) {
+                $this->logger->warning('Missing required fields', ['data' => $data]);
+                return new JsonResponse(['error' => 'Missing required fields'], Response::HTTP_BAD_REQUEST);
+            }
+
+            $user->setLogin($data['login']);
+            $user->setPassword($this->passwordHasher->hashPassword($user, $data['password']));
+            $user->setEmail($data['email']);
+            $user->setFirstname($data['firstname']);
+            $user->setLastname($data['lastname']);
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $responseData = [
+                'id' => $user->getId(),
+                'login' => $user->getLogin(),
+                'email' => $user->getEmail(),
+                'firstname' => $user->getFirstname(),
+                'lastname' => $user->getLastname(),
+            ];
+
+            $this->logger->info('Utilisateur enregistré', ['data' => $responseData]);
+            // Renvoie une réponse JSON avec le nouvel utilisateur créé avec un statut 201 (CREATED)
+            return new JsonResponse($responseData, Response::HTTP_CREATED);
+        } catch (\Exception $e) {
+            $this->logger->error('Erreur d\'enregistrement de l\'utilisateur : ' . $e->getMessage(), ['exception' => $e]);
+            return new JsonResponse(['error' => 'Échec de l\'enregistrement de l\'utilisateur'], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
-
-        $user->setLogin($data['login']);
-        $user->setPassword($this->passwordHasher->hashPassword($user, $data['password']));
-        $user->setEmail($data['email']);
-        $user->setFirstname($data['firstname']);
-        $user->setLastname($data['lastname']);
-
-        $entityManager->persist($user);
-        $entityManager->flush();
-
-        $responseData = [
-            'id' => $user->getId(),
-            'login' => $user->getLogin(),
-            'email' => $user->getEmail(),
-            'firstname' => $user->getFirstname(),
-            'lastname' => $user->getLastname(),
-        ];
-
-        // Renvoie une réponse JSON avec le nouvel utilisateur créé avec un statut 201 (CREATED)
-        return new JsonResponse($responseData, Response::HTTP_CREATED);
     }
 
     /**
@@ -79,23 +91,33 @@ class AuthController extends AbstractController
     #[OA\Response(response: 400, description: 'Invalid data')]
     public function login(Request $request, EntityManagerInterface $manager): JsonResponse
     {
+        $this->logger->info('Connexion de l\'utilisateur');
+
         if ($request->getMethod() === 'OPTIONS') {
+            $this->logger->info('OPTIONS request');
             return new JsonResponse(null, Response::HTTP_OK);
         }        
 
-        $data = json_decode($request->getContent(), true);
+        try {
+            $data = json_decode($request->getContent(), true);
+            $this->logger->debug('Request data', ['data' => $data]);
+            $user = $manager->getRepository(User::class)->findOneBy(['email' => $data['email']]);
 
-        $user = $manager->getRepository(User::class)->findOneBy(['email' => $data['email']]);
+            if (!$user || !$this->passwordHasher->isPasswordValid($user, $data['password'])) {
+                $this->logger->warning('L\'email ou le mot de passe sont invalides', ['data' => $data]);
+                return new JsonResponse(['error' => 'Invalid email or password'], Response::HTTP_BAD_REQUEST);
+            }
 
-        if (!$user || !$this->passwordHasher->isPasswordValid($user, $data['password'])) {
-            return new JsonResponse(['error' => 'Invalid credentials'], Response::HTTP_BAD_REQUEST);
+            $this->logger->debug('User found', ['user' => $user]);
+
+            $token = $this->tokenService->createToken($user->getId());
+            $this->logger->info('Token créé', ['token' => $token]);
+
+            return new JsonResponse(['token' => $token], Response::HTTP_OK);
+        } catch (\Exception $e) {
+            $this->logger->error('Erreur lors de la connexion de l\'utilisateur : ' . $e->getMessage());
+            return new JsonResponse(['error' => 'Échec de connexion de l\'utilisateur'], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
-
-        $token = $this->tokenService->createToken($user->getId());
-
-        $decodedToken = $this->tokenService->decodeToken($token);
-
-        return new JsonResponse(['token' => $token], Response::HTTP_OK);
     }
 
     /**
@@ -108,43 +130,52 @@ class AuthController extends AbstractController
     #[OA\RequestBody(content: new Model(type: UserType::class), required: true)]
     public function modifyUser(Request $request, EntityManagerInterface $manager): JsonResponse
     {
-        $data = json_decode($request->getContent(), true);
+        $this->logger->info('Modification de l\'utilisateur');
+        try {
+            $data = json_decode($request->getContent(), true);
+            $this->logger->debug('Request data', ['data' => $data]);
 
-        // Utilisation du service pour obtenir l'utilisateur
-        $result = $this->userService->getUserFromRequest($request);
-        if ($result instanceof JsonResponse) {
-            return $result;
-        }
+            // Utilisation du service pour obtenir l'utilisateur
+            $result = $this->userService->getUserFromRequest($request);
+            if ($result instanceof JsonResponse) {
+                $this->logger->warning('L\'utilisateur n\'a pas pu être retrouver', ['data' => $data]);
+                return $result;
+            }
 
-        $user = $result;
+            $user = $result;
 
-        if (isset($data['login'])) {
-            $user->setLogin($data['login']);
-        }
-        if (isset($data['password'])) {
-            $user->setPassword($this->passwordHasher->hashPassword($user, $data['password']));
-        }
-        if (isset($data['email'])) {
-            $user->setEmail($data['email']);
-        }
-        if (isset($data['firstname'])) {
-            $user->setFirstname($data['firstname']);
-        }
-        if (isset($data['lastname'])) {
-            $user->setLastname($data['lastname']);
-        }
+            if (isset($data['login'])) {
+                $user->setLogin($data['login']);
+            }
+            if (isset($data['password'])) {
+                $user->setPassword($this->passwordHasher->hashPassword($user, $data['password']));
+            }
+            if (isset($data['email'])) {
+                $user->setEmail($data['email']);
+            }
+            if (isset($data['firstname'])) {
+                $user->setFirstname($data['firstname']);
+            }
+            if (isset($data['lastname'])) {
+                $user->setLastname($data['lastname']);
+            }
 
-        $manager->flush();
+            $manager->flush();
 
-        $responseData = [
-            'id' => $user->getId(),
-            'login' => $user->getLogin(),
-            'email' => $user->getEmail(),
-            'firstname' => $user->getFirstname(),
-            'lastname' => $user->getLastname(),
-        ];
+            $responseData = [
+                'id' => $user->getId(),
+                'login' => $user->getLogin(),
+                'email' => $user->getEmail(),
+                'firstname' => $user->getFirstname(),
+                'lastname' => $user->getLastname(),
+            ];
 
-        return new JsonResponse($responseData, Response::HTTP_OK);
+            $this->logger->info('Utilisateur modifié', ['data' => $responseData]);
+            return new JsonResponse($responseData, Response::HTTP_OK);
+        } catch (\Exception $e) {
+            $this->logger->error('Erreur lors de la modification de l\'utilisateur : ' . $e->getMessage());
+            return new JsonResponse(['error' => 'Échec de modification de l\'utilisateur'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
    
     /**
@@ -155,31 +186,39 @@ class AuthController extends AbstractController
     #[OA\Response(response: 200, description: 'Returns the current user informations')]
     public function displayUserInformations(Request $request, SerializerInterface $serializer): Response
     {
-        // Utilisation du service pour obtenir l'utilisateur
-        $result = $this->userService->getUserFromRequest($request);
-        if ($result instanceof JsonResponse) {
-            return $result;
-        }
+        $this->logger->info('Affichage des informations de l\'utilisateur');
+        try {
+            // Utilisation du service pour obtenir l'utilisateur
+            $result = $this->userService->getUserFromRequest($request);
+            if ($result instanceof JsonResponse) {
+                $this->logger->warning('L\'utilisateur n\'a pas pu être retrouver');
+                return $result;
+            }
 
-        $user = $result;
+            $user = $result;
+            
+            $userWithoutPassword = new class {
+                public string $id;
+                public string $login;
+                public string $email;
+                public string $firstname;
+                public string $lastname;
+            };
         
-        $userWithoutPassword = new class {
-            public string $id;
-            public string $login;
-            public string $email;
-            public string $firstname;
-            public string $lastname;
-        };
-    
-        $userWithoutPassword->id = $user->getId();
-        $userWithoutPassword->login = $user->getLogin();
-        $userWithoutPassword->email = $user->getEmail();
-        $userWithoutPassword->firstname = $user->getFirstname();
-        $userWithoutPassword->lastname = $user->getLastname();
-    
-        $responseData = $serializer->normalize($userWithoutPassword);
-    
-        return $this->json($responseData, Response::HTTP_OK);
+            $userWithoutPassword->id = $user->getId();
+            $userWithoutPassword->login = $user->getLogin();
+            $userWithoutPassword->email = $user->getEmail();
+            $userWithoutPassword->firstname = $user->getFirstname();
+            $userWithoutPassword->lastname = $user->getLastname();
+        
+            $responseData = $serializer->normalize($userWithoutPassword);
+        
+            $this->logger->info('Informations de l\'utilisateur affichées', ['data' => $responseData]);
+            return $this->json($responseData, Response::HTTP_OK);
+        } catch (\Exception $e) {
+            $this->logger->error('Erreur lors de l\'affichage des informations de l\'utilisateur : ' . $e->getMessage());
+            return new JsonResponse(['error' => 'Échec de l\'affichage des informations de l\'utilisateur'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
     /**
