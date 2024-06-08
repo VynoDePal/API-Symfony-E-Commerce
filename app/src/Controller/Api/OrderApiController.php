eb81c8ecdd2b9325071a1d79e4478157f4a73d12
VynoDePal <odjokevyn@gmail.com>
2024-06-08 20:20:59 +0100

    feat: Order error management

--- app/src/Controller/Api/OrderApiController.php
+++ app/src/Controller/Api/OrderApiController.php
@@ -10,15 +10,18 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Psr\Log\LoggerInterface;
 use OpenApi\Attributes as OA;
 
 class OrderApiController extends AbstractController
 {
     private UserService $userService;
+    private LoggerInterface $logger;
 
-    public function __construct(UserService $userService)
+    public function __construct(UserService $userService, LoggerInterface $logger)
     {
         $this->userService = $userService;
+        $this->logger = $logger;
     }
 
     /**
@@ -31,25 +34,32 @@ class OrderApiController extends AbstractController
     #[OA\Response(response: 404, description: 'Not found')]
     public function getOrdersUser(Request $request, OrderRepository $orderRepository, NormalizerInterface $normalizer): Response
     {
-        /**
-         * Trouve l'utilisateur actuel en fonction de sa session
-         */
-        // Utilisation du service pour obtenir l'utilisateur
-        $result = $this->userService->getUserFromRequest($request);
-        if ($result instanceof JsonResponse) {
-            return $result;
+        $this->logger->info('Récupération des commandes de l\'utilisateur actuel.');
+
+        try {
+            // Utilisation du service pour obtenir l'utilisateur
+            $result = $this->userService->getUserFromRequest($request);
+            if ($result instanceof JsonResponse) {
+                return $result;
+            }
+
+            $user = $result;
+
+            // Recherche toutes les commandes de l'utilisateur actuel
+            $orders = $orderRepository->findBy(['user' => $user]);
+
+            if (empty($orders)) {
+                $this->logger->warning('L\'utilisateur n\'a pas de commandes');
+            }
+
+            $ordersJson = $normalizer->normalize($orders, 'json', ['groups' => ['orders', 'product:read']]);
+            $this->logger->info('Les commandes ont été récupérées avec succès.');
+
+            return $this->json($ordersJson);
+        } catch (\Exception $e) {
+            $this->logger->error('Une erreur s\'est produite lors de la recherche des commandes : ' . $e->getMessage());
+            return $this->json(['error' => 'Une erreur s\'est produite lors de la recherche des commandes.'], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
-
-        $user = $result;
-
-        /**
-         * Recherche toutes les commandes de l'utilisateur actuel
-         */
-        $orders = $orderRepository->findBy(['user' => $user]);
-
-        $ordersJson = $normalizer->normalize($orders, 'json', ['groups' => ['orders', 'product:read']]);
-
-        return $this->json($ordersJson);
     }
 
 
@@ -63,26 +73,33 @@ class OrderApiController extends AbstractController
     #[OA\Response(response: 404, description: 'Not found')]
     public function getOrderUser(Request $request, OrderRepository $orderRepository, NormalizerInterface $normalizer, int $id): Response
     {
-        // Utilisation du service pour obtenir l'utilisateur
-        $result = $this->userService->getUserFromRequest($request);
-        if ($result instanceof JsonResponse) {
-            return $result;
+        $this->logger->info('Récupération de la commande n°' . $id . ' de l\'utilisateur actuel.');
+
+        try {
+            // Utilisation du service pour obtenir l'utilisateur
+            $result = $this->userService->getUserFromRequest($request);
+            if ($result instanceof JsonResponse) {
+                return $result;
+            }
+
+            $user = $result;
+
+            // Recherche la commande correspondant à l'id envoyé en paramètre
+            $order = $orderRepository->findOneBy(['id' => $id, 'user' => $user]);
+
+            if (!$order) {
+                $this->logger->warning('La commande n\'a pas été trouvé');
+                return $this->json(['message' => 'Order not found'], 404);
+            }
+
+            $orderJson = $normalizer->normalize($order, 'json', ['groups' => ['orders', 'product:read']]);
+            $this->logger->info('La commande a été trouvé avec succès.');
+
+            return $this->json($orderJson);
+        } catch (\Exception $e) {
+            $this->logger->error('Une erreur s\'est produite lors de la recherche de la commande n°' . $id . ' : ' . $e->getMessage());
+            return $this->json(['error' => 'Une erreur s\'est produite lors de la recherche de la commande.'], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
-
-        $user = $result;
-
-        /**
-         * Recherche la commande correspondant à l'id envoyé en paramètre
-         */
-        $order = $orderRepository->findOneBy(['id' => $id, 'user' => $user]);
-
-        if (!$order) {
-            return $this->json(['message' => 'Order not found'], 404);
-        }
-
-        $orderJson = $normalizer->normalize($order, 'json', ['groups' => ['orders', 'product:read']]);
-
-        return $this->json($orderJson);
     }
 
 }
